ddbabd33defc5d4028280db6703272b7334c225f
josesousa <josesousa9000@gmail.com>
2015-03-19 14:42:12 +0000

    change name

--- Modelador/Modelador.h
+++
@@ -1,3 +0,0 @@
-void drawSphereXML(float r, int stacks, int slices,char *filename);
-void drawParallelpipedXML(float width, float height, float lenght);
-void drawPyramidXML(float base, float height);
