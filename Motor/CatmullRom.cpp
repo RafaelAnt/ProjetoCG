b43709562459b2975619c2bd1cf80b8409a4946c
Rafael Antunes <rafael.uminho@hotmail.com>
2015-05-01 00:43:58 +0100

    Points e Vectors to Array And Matrix

--- Motor/CatmullRom.cpp
+++ Motor/CatmullRom.cpp
@@ -15,7 +15,7 @@ float m[4][4] = { { -0.5f, 1.5f, -1.5f, 0.5f },
 { -0.5f, 0.0f, 0.5f, 0.0f },
 { 0.0f, 1.0f, 0.0f, 0.0f } };
 
-float EPSILON = 0.0001;
+/*float EPSILON = 0.0001;
 
 //Prototipos
 Point getCatmullRomPoint(float t, int *indices, vector<Point> p);
@@ -26,7 +26,7 @@ bool igual(float a, float b){
 }
 
 /* Metodo que dado um vector com 3 posições, mormaliza-o.
-*/
+*//*
 Point normalizeVector(Point vec){
 	GLfloat length = (float)sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
 	vec.x = vec.x / length;
@@ -36,7 +36,7 @@ Point normalizeVector(Point vec){
 }
 
 /*Função que calcula o produto vectorial de dois arrays de 3 elementos
-*/
+*//*
 Point prodVectorial(Point a, Point b){
 	Point res;
 	res.x = a.y * b.z - a.z * b.y;
@@ -219,17 +219,20 @@ void catmullRomCurveMovement(GLfloat gt, vector<Point> p){
 
 	r = prodVectorial(b, der);
 	printf("%f %f %f\n", r.x, r.y, r.z);
-	up = prodVectorial(r, der);
+	up = prodVectorial(der, r);
 	printf("%f %f %f\n", up.x, up.y, up.z);
 
 	der = normalizeVector(der);
 	up = normalizeVector(up);
 	r = normalizeVector(r);
 
+
+
 	GLfloat matriz[16] = { der.x, der.y, der.z, 0,
 		up.x, up.y, up.z, 0,
 		r.x, r.y, r.z, 0,
 		pos.x, pos.y, pos.z, 1.0f };
+
 
 	renderCatmullRomCurve(p);
 	glMultMatrixf(matriz);
@@ -239,16 +242,255 @@ void catmullRomCurveMovement(GLfloat gt, vector<Point> p){
 	GLUquadric* cy3 = gluNewQuadric();
 
 	glPushMatrix();
-	double len = 1.0;
-	glColor3f(1.0, 1.0, 1.0);
-	glBegin(GL_LINES);
-	glVertex3d(0, 0, 0);
-	glVertex3d(len, 0, 0);
-	glVertex3d(0, 0, 0);
-	glVertex3d(0, len, 0);
-	glVertex3d(0, 0, 0);
-	glVertex3d(0, 0, len);
-	glEnd();
-	glColor3f(0, 1.0, 0);
+		double len = 1.0;
+		glColor3f(1.0, 1.0, 1.0);
+		glBegin(GL_LINES);
+			glVertex3d(0, 0, 0);
+			glVertex3d(len, 0, 0);
+			glVertex3d(0, 0, 0);
+			glVertex3d(0, len, 0);
+			glVertex3d(0, 0, 0);
+			glVertex3d(0, 0, len);
+		glEnd();
+		glColor3f(0, 1.0, 0);
 	glPopMatrix();
+}*/
+
+//float p[200][3];
+
+#define POINT_COUNT 5
+// Points that make up the loop for catmull-rom interpolation
+float p[POINT_COUNT][3] = { { -1, -1, 0.5 }, { -1, 1, 0 }, { 1, 1.5, 0 }, { 2, 0, -1.5 }, { 1, -1, 0 } };
+
+
+
+//Prototipos:
+void renderCatmullRomCurve();
+void catmullRomCurveMovement(float gt, vector<Point> pontos);
+void getDerivadaAux(float t, int *indices, float *res);
+void getDerivada(float gt, float *res);
+void getCatmullRomPoint(float t, int *indices, float *res);
+void getGlobalCatmullRomPoint(float gt, float *res);
+void prodVectorial(float * a, float * b, float * res);
+void normalizeVector(float vec[]);
+
+
+
+void catmullRomCurveMovement(float gt, vector<Point> pontos){
+
+	/*POINT_COUNT = pontos.size();
+
+	for (int i = 0; i < POINT_COUNT; i++){
+		p[i][0] = pontos[i].x;
+		p[i][1] = pontos[i].y;
+		p[i][2] = pontos[i].z;
+	}*/
+
+	float pos[3], der[3], r[3], up[3], b[3] = { 0, 1, 0 };
+
+
+	renderCatmullRomCurve();
+
+
+	getGlobalCatmullRomPoint(gt, pos);
+	getDerivada(gt, der);
+
+	prodVectorial(der, b, r);
+	prodVectorial(r, der, up);
+
+	normalizeVector(der);
+	normalizeVector(up);
+	normalizeVector(r);
+
+	//desenhar eixo vertical
+	glBegin(GL_LINES);
+		glColor3f(1, 0, 0);
+		glVertex3f(pos[0], pos[1], pos[2]);
+		glVertex3f(pos[0] + up[0], pos[1] + up[1], pos[2] + up[2]);
+		glColor3f(1, 1, 1);
+	glEnd();
+
+	float matrizTeaPot[16] = { der[0], der[1], der[2], 0,
+		up[0], up[1], up[2], 0,
+		r[0], r[1], r[2], 0,
+		pos[0], pos[1], pos[2], 1.0f };
+
+
+	glMultMatrixf(matrizTeaPot);
+
+
+}
+
+
+void renderCatmullRomCurve() {
+
+	// desenhar a curva usando segmentos de reta - GL_LINE_LOOP
+
+	float res[3];
+	float vec[3];
+
+	glBegin(GL_LINE_LOOP);
+	for (float f = 0; f < 1; f += 0.01){
+		getGlobalCatmullRomPoint(f, res);
+		glVertex3f(res[0], res[1], res[2]);
+	}
+	glEnd();
+
+	for (float f = 0; f < 1; f += 0.01){
+		glBegin(GL_LINES);
+		getGlobalCatmullRomPoint(f, res);
+		glVertex3f(res[0], res[1], res[2]);
+		getDerivada(f, vec);
+		glVertex3f(res[0] + vec[0], res[1] + vec[1], res[2] + vec[2]);
+		glEnd();
+	}
+}
+
+
+void getDerivadaAux(float t, int *indices, float *res){
+	int sum = 0;
+
+	// M = catmull-rom matrix
+	float m[4][4] = { { -0.5f, 1.5f, -1.5f, 0.5f },
+	{ 1.0f, -2.5f, 2.0f, -0.5f },
+	{ -0.5f, 0.0f, 0.5f, 0.0f },
+	{ 0.0f, 1.0f, 0.0f, 0.0f } };
+
+	res[0] = 0.0; res[1] = 0.0; res[2] = 0.0;
+
+	// res diz a posição do objeto na curva?
+	// Compute point res = T * M * P
+
+	//preencher a matriz T
+	float matrizT[4] = { 3 * t*t, 2 * t, 1, 0 };
+
+	//preencher a matriz P
+	// where Pi = p[indices[i]]
+	float matrizP[4][3];
+
+	for (int i = 0; i < 4; i++){
+		for (int j = 0; j < 3; j++){
+			matrizP[i][j] = p[indices[i]][j];
+		}
+	}
+
+
+	float aux[4];
+	for (int i = 0; i < 4; i++) {
+		float sumAux = 0;
+		for (int j = 0; j < 4; j++) {
+			sumAux += matrizT[j] * m[j][i];
+
+		}
+		aux[i] = sumAux;
+	}
+
+	for (int i = 0; i < 3; i++) {
+		float sumAux = 0;
+		for (int j = 0; j < 4; j++) {
+			sumAux += aux[j] * matrizP[j][i];
+
+		}
+		res[i] = sumAux;
+	}
+}
+
+void getDerivada(float gt, float *res){
+
+	float t = gt * POINT_COUNT; // this is the real global t
+	int index = floor(t);  // which segment
+	t = t - index; // where within  the segment
+
+	// indices store the points
+	int indices[4];
+	indices[0] = (index + POINT_COUNT - 1) % POINT_COUNT;
+	indices[1] = (indices[0] + 1) % POINT_COUNT;
+	indices[2] = (indices[1] + 1) % POINT_COUNT;
+	indices[3] = (indices[2] + 1) % POINT_COUNT;
+
+	getDerivadaAux(t, indices, res);
+}
+
+
+void getCatmullRomPoint(float t, int *indices, float *res) {
+
+	int sum = 0;
+
+	// M = catmull-rom matrix
+	float m[4][4] = { { -0.5f, 1.5f, -1.5f, 0.5f },
+	{ 1.0f, -2.5f, 2.0f, -0.5f },
+	{ -0.5f, 0.0f, 0.5f, 0.0f },
+	{ 0.0f, 1.0f, 0.0f, 0.0f } };
+
+	res[0] = 0.0; res[1] = 0.0; res[2] = 0.0;
+
+	// res diz a posição do objeto na curva?
+	// Compute point res = T * M * P
+
+	//preencher a matriz T
+	float matrizT[4] = { t*t*t, t*t, t, 1 };
+
+	//preencher a matriz P
+	// where Pi = p[indices[i]]
+	float matrizP[4][3];
+
+	for (int i = 0; i < 4; i++){
+		for (int j = 0; j < 3; j++){
+			matrizP[i][j] = p[indices[i]][j];
+		}
+	}
+
+
+	float aux[4];
+	for (int i = 0; i < 4; i++) {
+		float sumAux = 0;
+		for (int j = 0; j < 4; j++) {
+			sumAux += matrizT[j] * m[j][i];
+
+		}
+		aux[i] = sumAux;
+	}
+
+	for (int i = 0; i < 3; i++) {
+		float sumAux = 0;
+		for (int j = 0; j < 4; j++) {
+			sumAux += aux[j] * matrizP[j][i];
+
+		}
+		res[i] = sumAux;
+	}
+
+
+}
+
+
+// given  global t, returns the point in the curve
+void getGlobalCatmullRomPoint(float gt, float *res) {
+
+	float t = gt * POINT_COUNT; // this is the real global t
+	int index = floor(t);  // which segment
+	t = t - index; // where within  the segment
+
+	// indices store the points
+	int indices[4];
+	indices[0] = (index + POINT_COUNT - 1) % POINT_COUNT;
+	indices[1] = (indices[0] + 1) % POINT_COUNT;
+	indices[2] = (indices[1] + 1) % POINT_COUNT;
+	indices[3] = (indices[2] + 1) % POINT_COUNT;
+
+	getCatmullRomPoint(t, indices, res);
+}
+
+void prodVectorial(float * a, float * b, float * res){
+	res[0] = (a[1] * b[2]) - (a[2] * b[1]);
+	res[1] = -((a[0] * b[2]) - (a[2] * b[0]));
+	res[2] = (a[0] * b[1]) - (a[1] * b[0]);
+}
+
+void normalizeVector(float vec[]){
+	float aux = vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2];
+	float aux2 = sqrt(aux);
+	vec[0] = vec[0] / aux2;
+	vec[1] = vec[1] / aux2;
+	vec[2] = vec[2] / aux2;
 }
