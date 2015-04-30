e50e20c81348b8de250cc9a6d2979c688c1fea0b
josesousa <josesousa9000@gmail.com>
2015-04-30 23:51:39 +0100

    debug axis

--- Motor/CatmullRom.cpp
+++ Motor/CatmullRom.cpp
@@ -15,10 +15,15 @@ float m[4][4] = { { -0.5f, 1.5f, -1.5f, 0.5f },
 { -0.5f, 0.0f, 0.5f, 0.0f },
 { 0.0f, 1.0f, 0.0f, 0.0f } };
 
+float EPSILON = 0.0001;
 
 //Prototipos
 Point getCatmullRomPoint(float t, int *indices, vector<Point> p);
 
+bool igual(float a, float b){
+	float diff = a - b;
+	return (diff < EPSILON) && (-diff < EPSILON);
+}
 
 /* Metodo que dado um vector com 3 posições, mormaliza-o.
 */
@@ -34,9 +39,9 @@ Point normalizeVector(Point vec){
 */
 Point prodVectorial(Point a, Point b){
 	Point res;
-	res.x = (a.y * b.z) - (a.z * b.y);
-	res.y = -((a.x * b.z) - (a.z * b.x));
-	res.z = (a.x * b.y) - (a.y * b.x);
+	res.x = a.y * b.z - a.z * b.y;
+	res.y = a.z * b.x - a.x * b.z;
+	res.z = a.x * b.y - a.y * b.x;
 	return res;
 }
 
@@ -83,11 +88,9 @@ Point getCatmullRomPoint(float t, int *indices, vector<Point> p) {
 	float matrizP[4][3];
 
 	for (int i = 0; i < 4; i++){
-		for (int j = 0; j < 3; j++){
-			if (j == 0)matrizP[i][j] = p[indices[i]].x;
-			if (j == 1)matrizP[i][j] = p[indices[i]].y;
-			if (j == 2)matrizP[i][j] = p[indices[i]].z;
-		}
+		matrizP[i][0] = p[indices[i]].x;
+		matrizP[i][1] = p[indices[i]].y;
+		matrizP[i][2] = p[indices[i]].z;
 	}
 
 
@@ -118,10 +121,7 @@ Point getCatmullRomPoint(float t, int *indices, vector<Point> p) {
 Point getDerivadaAux(float t, int *indices, vector<Point> p) {
 
 	int sum = 0;
-	Point res;// res diz a posição do objeto na curva
-
-	// M = catmull-rom matrix
-
+	Point res;
 
 	res.x = 0.0;
 	res.y = 0.0;
@@ -130,18 +130,16 @@ Point getDerivadaAux(float t, int *indices, vector<Point> p) {
 	// Compute point res = T * M * P
 
 	//preencher a matriz T
-	float matrizT[4] = { 3*t*t, 2*t, 1, 0 };
+	float matrizT[4] = { 3 * t*t, 2 * t, 1, 0 };
 
 	//preencher a matriz P
 	// where Pi = p[indices[i]]
 	float matrizP[4][3];
 
 	for (int i = 0; i < 4; i++){
-		for (int j = 0; j < 3; j++){
-			if (j == 0)matrizP[i][j] = p[indices[i]].x;
-			if (j == 1)matrizP[i][j] = p[indices[i]].y;
-			if (j == 2)matrizP[i][j] = p[indices[i]].z;
-		}
+		matrizP[i][0] = p[indices[i]].x;
+		matrizP[i][1] = p[indices[i]].y;
+		matrizP[i][2] = p[indices[i]].z;
 	}
 
 
@@ -201,31 +199,32 @@ void renderCatmullRomCurve(vector<Point> p) {
 	}
 	glEnd();
 
-	/*for (float f = 0; f < 1; f += 0.01){
-	glBegin(GL_LINES);
-	getGlobalCatmullRomPoint(f, res);
-	glVertex3f(res[0], res[1], res[2]);
-	getDerivada(f, vec);
-	glVertex3f(res[0] + vec[0], res[1] + vec[1], res[2] + vec[2]);
-	glEnd();
-	}*/
+	for (float f = 0; f < 1; f += 0.01){
+		glBegin(GL_LINES);
+		Point res = getGlobalCatmullRomPoint(f, p);
+		glVertex3f(res.x, res.y, res.z);
+		Point dev = getDerivada(f, p);
+		glVertex3f(res.x + dev.x, res.y + dev.y, res.z + dev.z);
+		glEnd();
+	}
 }
 
 void catmullRomCurveMovement(GLfloat gt, vector<Point> p){
 
 	Point pos, der, r, up, b = { 0, 1, 0 };
 
-	pos=getGlobalCatmullRomPoint(gt, p);
+	pos = getGlobalCatmullRomPoint(gt, p);
 
-	der=getDerivada(gt, p);
+	der = getDerivada(gt, p);
 
-	r=prodVectorial(der, b);
+	r = prodVectorial(b, der);
+	printf("%f %f %f\n", r.x, r.y, r.z);
+	up = prodVectorial(r, der);
+	printf("%f %f %f\n", up.x, up.y, up.z);
 
-	up=prodVectorial(r, der);
-
-	der=normalizeVector(der);
-	up=normalizeVector(up);
-	r=normalizeVector(r);
+	der = normalizeVector(der);
+	up = normalizeVector(up);
+	r = normalizeVector(r);
 
 	GLfloat matriz[16] = { der.x, der.y, der.z, 0,
 		up.x, up.y, up.z, 0,
@@ -235,4 +234,21 @@ void catmullRomCurveMovement(GLfloat gt, vector<Point> p){
 	renderCatmullRomCurve(p);
 	glMultMatrixf(matriz);
 
+	GLUquadric* cyl = gluNewQuadric();
+	GLUquadric* cy2 = gluNewQuadric();
+	GLUquadric* cy3 = gluNewQuadric();
+
+	glPushMatrix();
+	double len = 1.0;
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_LINES);
+	glVertex3d(0, 0, 0);
+	glVertex3d(len, 0, 0);
+	glVertex3d(0, 0, 0);
+	glVertex3d(0, len, 0);
+	glVertex3d(0, 0, 0);
+	glVertex3d(0, 0, len);
+	glEnd();
+	glColor3f(0, 1.0, 0);
+	glPopMatrix();
 }
