21508ee950094d253d7f85ebee7ce29f9b3fbcd9
josesousa <josesousa9000@gmail.com>
2015-05-04 10:08:54 +0100

    cleanup code

--- Motor/CatmullRom.cpp
+++ Motor/CatmullRom.cpp
@@ -10,20 +10,15 @@ using namespace std;
 
 
 // catmull-rom matrix
-float m[4][4] = { { -0.5f, 1.5f, -1.5f, 0.5f },
-{ 1.0f, -2.5f, 2.0f, -0.5f },
-{ -0.5f, 0.0f, 0.5f, 0.0f },
-{ 0.0f, 1.0f, 0.0f, 0.0f } };
+static float m[4][4] = { { -0.5f, 1.5f, -1.5f, 0.5f},
+						 { 1.0f, -2.5f, 2.0f, -0.5f},
+						 { -0.5f, 0.0f, 0.5f, 0.0f},
+						 { 0.0f, 1.0f, 0.0f, 0.0f}};
 
+static float EPSILON = 0.0001;
+//Metodo que dado um vector, mormaliza-o.
 
-float EPSILON = 0.0001;
-
-//Prototipos
-Point getCatmullRomPoint(float t, int *indices, vector<Point> p);
-
-//Metodo que dado um vector com 3 posições, mormaliza-o.
-
-Point normalizeVector(Point vec){
+static Point normalizeVector(Point vec){
 	GLfloat length = (float)sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
 	vec.x = vec.x / length;
 	vec.y = vec.y / length;
@@ -31,42 +26,14 @@ Point normalizeVector(Point vec){
 	return vec;
 }
 
-//Função que calcula o produto vectorial de dois arrays de 3 elementos
-
-//dado um t global calcular o ponto na curva
-Point getGlobalCatmullRomPoint(GLfloat gt, vector<Point> p) {
-
-	int POINT_COUNT = p.size();
-
-	float t = gt * POINT_COUNT; //obter t global verdadeiro
-	int index = floor(t);  //segmento
-	t = t - index; //posição no segmento
-
-	//obter os indices dos nossos pontos de controlo
-	int indices[4];
-	indices[0] = (index + POINT_COUNT - 1) % POINT_COUNT;
-	indices[1] = (indices[0] + 1) % POINT_COUNT;
-	indices[2] = (indices[1] + 1) % POINT_COUNT;
-	indices[3] = (indices[2] + 1) % POINT_COUNT;
-
-	return getCatmullRomPoint(t, indices, p);
-
-}
-
-
-Point getCatmullRomPoint(float t, int *indices, vector<Point> p) {
+//obtem um ponto num segmento da curva
+static Point getCatmullRomPoint(float t, int *indices, vector<Point> p) {
 
 	int sum = 0;
 	Point res;// res diz a posição do objeto na curva
-
-	// M = catmull-rom matrix
-
-
 	res.x = 0.0;
 	res.y = 0.0;
 	res.z = 0.0;
-
-	// Compute point res = T * M * P
 
 	//preencher a matriz T
 	float matrizT[4] = { t*t*t, t*t, t, 1 };
@@ -105,6 +72,27 @@ Point getCatmullRomPoint(float t, int *indices, vector<Point> p) {
 	return res;
 }
 
+//dado um t global calcular o ponto na curva
+static Point getGlobalCatmullRomPoint(GLfloat gt, vector<Point> p) {
+
+	int POINT_COUNT = p.size();
+
+	float t = gt * POINT_COUNT; //obter t global verdadeiro
+	int index = floor(t);  //segmento
+	t = t - index; //posição no segmento
+
+	//obter os indices dos nossos pontos de controlo
+	int indices[4];
+	indices[0] = (index + POINT_COUNT - 1) % POINT_COUNT;
+	indices[1] = (indices[0] + 1) % POINT_COUNT;
+	indices[2] = (indices[1] + 1) % POINT_COUNT;
+	indices[3] = (indices[2] + 1) % POINT_COUNT;
+
+	return getCatmullRomPoint(t, indices, p);
+
+}
+
+//calcula um ponto na curva definida pelos pontos de controlo p, num instante gt [0,1]
 void catmullRomCurveMovement(GLfloat gt, vector<Point> p){
 
 	Point pos, b = { 0, 1, 0 };
